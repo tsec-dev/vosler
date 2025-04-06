d182edd995241564f88c2ad0bad6bb57043f815e
tsec-dev <taylor@tsec.dev>
2025-04-06 17:30:06 -0400

    More Sign-in fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.variable} ${sourceCodePro.variable} antialiased`}>
+    <html lang="en">
+      <body className={`${inter.variable} ${sourceCodePro.variable} antialiased`}>
+        <ClerkProvider>
           {children}
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
