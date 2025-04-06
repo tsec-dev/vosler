876dda1e2dd2464ffadfe05f2e678d8943d6aa00
tsec-dev <taylor@tsec.dev>
2025-04-06 17:46:24 -0400

    Fixin it up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/sign-in");
+export default async function Home() {
+  const user = await currentUser();
+  if (user) {
+    // If authenticated, send them to dashboard
+    redirect("/dashboard");
+  } else {
+    // Otherwise, send them to sign-in
+    redirect("/sign-in");
+  }
 }
