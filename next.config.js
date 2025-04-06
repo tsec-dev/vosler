4cfe824e5a523c65b974e929ba0bda0a3c021c3e
tsec-dev <taylor@tsec.dev>
2025-04-05 20:47:37 -0400

    Move landing page to src/app/page.tsx and fix route

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your configuration options here
+  reactStrictMode: true,
+  // Any other options...
+};
+
+module.exports = nextConfig;
