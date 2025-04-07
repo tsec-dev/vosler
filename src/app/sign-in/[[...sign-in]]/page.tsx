2bd3050bc7b5e75558880031f979c5aa58ff60c0
tsec-dev <taylor@tsec.dev>
2025-04-06 20:13:18 -0400

    Fixin it up defsuldft

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { SignIn } from "@clerk/nextjs";
+import { ClerkProvider, SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <SignIn redirectUrl="/dashboard" />
-    </div>
+    <ClerkProvider>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <SignIn redirectUrl="/dashboard" />
+      </div>
+    </ClerkProvider>
   );
 }
