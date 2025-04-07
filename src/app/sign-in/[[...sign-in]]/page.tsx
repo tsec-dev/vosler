b7f7ccccddb4b5b9e6b092b8d85b4f3a800d36f7
tsec-dev <taylor@tsec.dev>
2025-04-06 20:17:45 -0400

    Signin Simplified

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -5,8 +5,13 @@ import { ClerkProvider, SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <ClerkProvider>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <SignIn redirectUrl="/dashboard" />
+      <div className="min-h-screen relative flex items-center justify-center">
+        {/* Background gradient */}
+        <div className="absolute inset-0 bg-gradient-to-b from-[#0a1933] to-[#101726]" />
+        {/* Centered SignIn widget */}
+        <div className="relative z-10">
+          <SignIn redirectUrl="/dashboard" />
+        </div>
       </div>
     </ClerkProvider>
   );
