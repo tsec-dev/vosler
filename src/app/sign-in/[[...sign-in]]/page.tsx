aa50f679db0e20d1de6bcc7565a841bd86213932
tsec-dev <taylor@tsec.dev>
2025-04-06 20:24:59 -0400

    Signin Dark

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { ClerkProvider, SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function SignInPage() {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <div className="min-h-screen relative flex items-center justify-center">
         {/* Background gradient */}
         <div className="absolute inset-0 bg-gradient-to-b from-[#0a1933] to-[#101726]" />
