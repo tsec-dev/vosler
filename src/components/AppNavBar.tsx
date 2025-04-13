215ff2c59a4e3a234baa752d9b07586b8e81e9d0
tsec-dev <taylor@tsec.dev>
2025-04-12 23:23:01 -0600

    Class Man

--- src/components/AppNavBar.tsx
+++ src/components/AppNavBar.tsx
@@ -53,10 +53,7 @@ export default function AppNavBar({ isAdmin, showBackToDashboard }: AppNavBarPro
         {isAdmin && (
           <>
             <button className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-500 text-sm font-medium transition">
-              Export to PDF
-            </button>
-            <button className="px-4 py-2 rounded-md bg-red-600 text-white hover:bg-red-500 text-sm font-medium transition">
-              Reset Class
+              EXPORT
             </button>
           </>
         )}
